dbb3092fe8379ee487167609b2956de4cd022ae5
jbardell19 <catchrb19@gmail.com>
2019-10-07 11:52:50 -0500

    Location fix JS

--- myPortfolio/src/dashboard.js
+++ myPortfolio/src/dashboard.js
@@ -11,3 +11,21 @@ $(document).ready(function() {
     }
   });
 });
+
+var myLocation = document.getElementById("my-coordinates");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(displayPosition);
+  } else {
+    myLocation.innerHTML = "Not available";
+  }
+}
+
+function displayPosition(position) {
+  myLocation.innerHTML =
+    "Longitude: " +
+    position.coords.longitude +
+    " Latitude: " +
+    position.coords.latitude;
+}
